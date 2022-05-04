2f585a6cb454f286d31026b053c3168ecb7ffc81
kesepkina <k.esepkina@gmail.com>
2022-05-04 16:20:31 +0300

    KNN and RandomForest models added

--- src/forest_cover_type/pipeline.py
+++ src/forest_cover_type/pipeline.py
@@ -1,20 +1,42 @@
+from scipy import rand
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def create_pipeline(
-    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
+    use_scaler: bool,
+    clf: str,
+    **params
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
+    if clf == 'logreg':
+        classifier = LogisticRegression(
+                random_state=params['random_state'],
+                max_iter=params['max_iter'],
+                C=params['logreg_c']
+            )
+    elif clf == 'knn':
+        classifier = KNeighborsClassifier(
+            n_neighbors=params['n_neighbors'],
+            weights=params['weights']
+        )
+    elif clf == 'forest':
+        classifier = RandomForestClassifier(
+            n_estimators=params['n_estimators'],
+            criterion=params['criterion'],
+            max_depth=params['max_depth'],
+            bootstrap=params['bootstrap'],
+            random_state=params['random_state']
+        )
     pipeline_steps.append(
         (
             "classifier",
-            LogisticRegression(
-                random_state=random_state, max_iter=max_iter, C=logreg_C
-            ),
+            classifier,
         )
     )
     return Pipeline(steps=pipeline_steps)
--- src/forest_cover_type/train.py
+++ src/forest_cover_type/train.py
@@ -11,8 +11,39 @@ import numpy as np
 from .data import get_dataset, get_train_test_splits
 from .pipeline import create_pipeline
 
+def train_model(
+    dataset_path: Path,
+    save_model_path: Path,
+    use_scaler: bool,
+    clf: str,
+    **model_params
+) -> None:
+    features, target = get_dataset(
+        dataset_path
+    )
+    with mlflow.start_run():
+        pipeline = create_pipeline(use_scaler, clf, **model_params)
+        cv_accuracy = cross_val_score(pipeline, features, target, scoring='accuracy').mean()
+        cv_f1 = cross_val_score(pipeline, features, target, \
+                                scoring='f1_micro').mean()
+        cv_log_loss = cross_val_score(pipeline, features, target, scoring='neg_log_loss').mean()
+        mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_params(model_params)
+        mlflow.log_metric("accuracy_cv", cv_accuracy)
+        mlflow.log_metric("f1_score_cv", cv_f1)
+        mlflow.log_metric("Neg_log_loss_score_cv", cv_log_loss)
+        click.echo(f"Accuracy (CV): {cv_accuracy}.")
+        click.echo(f"F1 score (CV): {cv_f1}.")
+        click.echo(f"Negative Log loss score (CV): {cv_log_loss}.")
+        dump(pipeline, save_model_path)
+        click.echo(f"Model is saved to {save_model_path}.")
+        return
 
-@click.command()
+@click.group()
+def train():
+    pass
+
+@train.command('logreg')
 @click.option(
     "-d",
     "--dataset-path",
@@ -28,21 +59,15 @@ from .pipeline import create_pipeline
     show_default=True,
 )
 @click.option(
-    "--random-state",
-    default=42,
-    type=int,
-    show_default=True,
-)
-@click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
-    show_default=True,
-)
-@click.option(
     "--use-scaler",
     default=True,
     type=bool,
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
     show_default=True,
 )
 @click.option(
@@ -57,42 +82,129 @@ from .pipeline import create_pipeline
     type=float,
     show_default=True,
 )
-def train(
+@click.option(
+    "--n-estimators",
+    default=100,
+    type=int,
+    show_default=True,
+)
+def train_logreg(
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
-    test_split_ratio: float,
     use_scaler: bool,
     max_iter: int,
     logreg_c: float,
 ) -> None:
-    features, target = get_dataset(
-        dataset_path
-    )
-    # features_train, features_val, target_train, target_val = train_test_split(
-    #     features,
-    #     target,
-    #     random_state,
-    #     test_split_ratio
-    # )
-    with mlflow.start_run():
-        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
-        # pipeline.fit(features_train, target_train)
-        # y_pred = pipeline.predict(features_val)
-        # accuracy = accuracy_score(target_val, y_pred)
-        cv_accuracy = cross_val_score(pipeline, features, target, scoring='accuracy').mean()
-        cv_f1 = cross_val_score(pipeline, features, target, \
-                                scoring='f1_micro').mean()
-        cv_log_loss = cross_val_score(pipeline, features, target, scoring='neg_log_loss').mean()
-        # f1 = f1_score(target_val, y_pred)
-        mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
-        mlflow.log_metric("accuracy_cv", cv_accuracy)
-        mlflow.log_metric("f1_score_cv", cv_f1)
-        mlflow.log_metric("Neg_log_loss_score_cv", cv_log_loss)
-        click.echo(f"Accuracy (CV): {cv_accuracy}.")
-        click.echo(f"F1 score (CV): {cv_f1}.")
-        click.echo(f"Negative Log loss score (CV): {cv_log_loss}.")
-        dump(pipeline, save_model_path)
-        click.echo(f"Model is saved to {save_model_path}.")
+    model_params={'random_state': random_state, 'max_iter': max_iter, 'logreg_c': logreg_c}
+    train_model(dataset_path, save_model_path, use_scaler, 'logreg', **model_params)
+
+
+@train.command('knn')
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--n-neighbors",
+    default=5,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--weights",
+    default="uniform",
+    type=click.Choice(['uniform', 'distance']),
+    show_default=True,
+)
+def train_knn(
+    dataset_path: Path,
+    save_model_path: Path,
+    use_scaler: bool,
+    n_neighbors: int,
+    weights: str,
+) -> None:
+    model_params={'n_neighbors': n_neighbors, 'weights': weights}
+    train_model(dataset_path=dataset_path, save_model_path=save_model_path, use_scaler=use_scaler,
+            clf='knn', **model_params)
+
+@train.command('forest')
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--n-estimators",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--criterion",
+    default='gini',
+    type=click.Choice(['gini', 'entropy']),
+    show_default=True,
+)
+@click.option(
+    "--max-depth",
+    default=None,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--bootstrap",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+def train_forest(
+    dataset_path: Path,
+    save_model_path: Path,
+    use_scaler: bool,
+    n_estimators: int,
+    criterion: str,
+    max_depth: int,
+    bootstrap: bool,
+    random_state: int
+) -> None:
+    model_params={'n_estimators': n_estimators, 'criterion': criterion, 'max_depth': max_depth,
+                    'bootstrap': bootstrap, 'random_state': random_state}
+    train_model(dataset_path, save_model_path, use_scaler, 'forest', **model_params)
