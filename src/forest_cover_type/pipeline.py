ac8a5671f841b7fd009771f6c5001a85ae7226c6
kesepkina <k.esepkina@gmail.com>
2022-05-06 16:57:09 +0300

    Black and flake8 libraries added

--- src/forest_cover_type/pipeline.py
+++ src/forest_cover_type/pipeline.py
@@ -1,5 +1,3 @@
-from xml.sax.handler import feature_external_ges
-from scipy import rand
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
@@ -9,46 +7,40 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def create_pipeline(
-    scaler: str,
-    feature_eng: str,
-    clf: str,
-    **params
+    scaler: str, feature_eng: str, clf: str, **params
 ) -> Pipeline:
     pipeline_steps = []
-    if scaler == 'st_scaler':
+    if scaler == "st_scaler":
         sc = StandardScaler()
-    elif scaler == 'minmax_scaler':
+    elif scaler == "minmax_scaler":
         sc = MinMaxScaler()
     if scaler is not None:
-        pipeline_steps.append(('scaler', sc))
+        pipeline_steps.append(("scaler", sc))
 
-    if feature_eng == 'pca2':
+    if feature_eng == "pca2":
         preprocessor = PCA(2)
         pipeline_steps.append(("feature_eng", preprocessor))
-    elif feature_eng == 'pca3':
+    elif feature_eng == "pca3":
         preprocessor = PCA(3)
         pipeline_steps.append(("feature_eng", preprocessor))
-    # if feature_eng is not None:
-    #     pipeline_steps.append(("feature_eng", preprocessor))
 
-    if clf == 'logreg':
+    if clf == "logreg":
         classifier = LogisticRegression(
-                random_state=params['random_state'],
-                max_iter=params['max_iter'],
-                C=params['logreg_c']
-            )
-    elif clf == 'knn':
-        classifier = KNeighborsClassifier(
-            n_neighbors=params['n_neighbors'],
-            weights=params['weights']
+            random_state=params["random_state"],
+            max_iter=params["max_iter"],
+            C=params["logreg_c"],
         )
-    elif clf == 'forest':
+    elif clf == "knn":
+        classifier = KNeighborsClassifier(
+            n_neighbors=params["n_neighbors"], weights=params["weights"]
+        )
+    elif clf == "forest":
         classifier = RandomForestClassifier(
-            n_estimators=params['n_estimators'],
-            criterion=params['criterion'],
-            max_depth=params['max_depth'],
-            bootstrap=params['bootstrap'],
-            random_state=params['random_state']
+            n_estimators=params["n_estimators"],
+            criterion=params["criterion"],
+            max_depth=params["max_depth"],
+            bootstrap=params["bootstrap"],
+            random_state=params["random_state"],
         )
     pipeline_steps.append(
         (
